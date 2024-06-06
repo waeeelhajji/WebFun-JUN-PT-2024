5112a7f8f257d092b08e5d41ba3bd4eb35580294
waeeelhajji <waelhajji.contact@gmail.com>
2024-06-06 20:21:46 +0100

    Week2Day2

---
+++ Week2Day2/script.js
@@ -0,0 +1,33 @@
+function Hello(){
+    console.log("Hello from the Script.js")
+    console.log('hello')
+    var num = 5 + 6
+    console.log(num)
+}
+
+function ShowThis(element) {
+    console.log(element.innerHTML)
+    var num = 5 + 6
+    element.innerHTML = "Hello JS"
+}
+
+function turnOff(element) {
+    if(element.innerHTML == "On"){
+        element.innerText = "Off";
+        element.style.backgroundColor = "red"
+
+    }else {
+        element.innerHTML = "On"
+        element.style.backgroundColor = "#a0c3f4"
+    }
+}
+
+function DeleteElement(element){
+    element.innerHTML = "Hello onmouseover"
+    element.style.backgroundColor = "green"
+}
+
+function ShowAnotherThisg(element) {
+    element.innerHTML = "Hello onmouseout"
+    element.style.backgroundColor = "red"
+}
